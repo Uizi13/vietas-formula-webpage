f6daf9ecaa7f457d70ef7b33975777083a366990
Uizi13 <szymon.pl.jank@gmail.com>
2024-09-28 00:33:51 +0200

    fix

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const max_root = 10;
 function onPageLoad() {
     let x = [];
     while (x.length < degree) {
-        const r = Math.floor(Math.random * max_root * 2) - 10;
-        if (r != 0 && !x.includes(r)) {
+        const r = Math.floor(Math.random() * max_root * 2) - 10;
+        if (r !== 0 && !x.includes(r)) {
             x.push(r);
         }
     }
