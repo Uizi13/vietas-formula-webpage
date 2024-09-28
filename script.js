b539a4031a13e3b78ac0390e82ed63ea4848622f
Uizi13 <szymon.pl.jank@gmail.com>
2024-09-28 22:13:48 +0200

    coded combinations for polynomial multiplication

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ const max_root = 10;
 
 const main_text = document.getElementById("main_text");
 
+function combinations(arr, len) {
+    if (len > arr.length) {
+        return null;
+    }
+    let indices = Array.from(Array(len).keys());
+    let result = [arr.slice(0, len)];
+    while (true) {
+        let chosen_i = -1;
+        for (let i = len; i >= 0; i--) {
+            if (indices[i] !== i + n - len) {
+                chosen_i = i;
+                break;
+            }
+        }
+        if (chosen_i == -1) {
+            return result;
+        }
+        indices[chosen_i] += 1;
+        for (let j = chosen_i + 1; len; j++) {
+            indices[j] = indices[j - 1] + 1;
+        }
+        result.push(indices.map(i => arr[i]))
+    }
+}
+
 function onPageLoad() {
     let x = [];
     while (x.length < degree) {
@@ -13,8 +38,9 @@ function onPageLoad() {
     }
     console.log(x);
     for (let i = 0; i <= degree; i++) {
-        arr = [];
-        for (prod in combinations) {
+        const cbn = combinations(x, i);
+        let arr = [];
+        for (prod in cbn) {
             arr.push(prod.reduce((a, b) => a * b, 0))
         }
         console.log(-1 * arr.reduce((a, b) => a + b, 0) * ((i % 2) * 2 - 1))
